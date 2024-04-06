fe8412e2f3bd7e606c70b4c6a213697c80b50e44
Amit <akrnayak53220@gmail.com>
2024-04-06 23:31:28 +0530

    1st commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+// let score=true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// "33" --> 33
+
+//"22abc" --> NaN
+
+//true  -->     1;  false   --> 0
+
+
+// let isLogin=1
+
+// let booleanNumber = Boolean(isLogin)
+// console.log(booleanNumber);
+
+//convert to String
+
+// let someNumber=55
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************** Operation **********************/
+
+// let value =3
+// let negValue = -value
+//console.log(negValue);
+
+// let str1="Hello"
+// let str2=" Amit"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+1+"3"+4+5+"6"+7+8);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// ++gameCounter
+console.log(gameCounter++);
+console.log(gameCounter);
