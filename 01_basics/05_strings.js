92929f7b5c72780ac01d28d7040222de501d694b
Amit <akrnayak53220@gmail.com>
2024-04-07 11:55:53 +0530

    string script

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,51 @@
+//replace() method
+/*
+let fruits = "Apple, Banana, Orange, Banana, ApPle"
+
+let newFruit = fruits.replaceAll("Banana", "Dalimba")
+
+console.log(newFruit);
+*/
+//If you don't want to use /g flag then use replaceAll() method.
+
+// let anotherF=fruits.replceAll("Banana", "Kela")
+// console.log(anotherF);
+
+/*
+    padStart()
+    ----------
+*/
+
+// let text = "6"
+
+// let padText = text.padStart(4, "0")
+// let padText = text.padEnd(4, "0")
+
+// console.log(padText);
+
+
+/*
+    Convert String into Array
+    -------------------------
+*/
+
+// let myString = "a,b,c,d,e,f"
+// let myString2 = "abcde"
+
+// let myArray = myString.split(",")
+// let myArray2 = myString2.split("")
+// console.log(myArray);
+// console.log("new Array2");
+// console.log(myArray2);
+
+
+/*
+    concat or replace
+*/
+
+// let name = "Shadow"
+// let age = 25
+
+// let paragraph = `My name is ${name}. And I am ${age} years old.`
+
+// console.log(paragraph);
